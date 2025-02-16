6a4cea516613b19bb76f0a447c046f5edd9e84aa
thelma-github <thelmabuabeng4@gmail.com>
2025-02-16 10:37:11 +0000

    Update App.jsx
    // Import existing component
    import WelcomeMessage from './components/WelcomeMessage';
    
    // Import new components
    import Header from './Header';
    import MainContent from './MainContent';
    import Footer from './Footer';
    
    function App() {
        return (
            <div>
               <WelcomeMessage />
                <Header />
                <MainContent />
                <Footer />
            </div>
        );
    }
    
    export default App;

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,9 +1,18 @@
+// Import existing component
 import WelcomeMessage from './components/WelcomeMessage';
+
+// Import new components
+import Header from './Header';
+import MainContent from './MainContent';
+import Footer from './Footer';
 
 function App() {
     return (
         <div>
-            <WelcomeMessage />
+           <WelcomeMessage />
+            <Header />
+            <MainContent />
+            <Footer />
         </div>
     );
 }
