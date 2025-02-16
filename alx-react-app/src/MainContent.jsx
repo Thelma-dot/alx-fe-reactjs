0c56d6ec1b38404cbedfbf54c7ecefe4be10a424
thelma-github <thelmabuabeng4@gmail.com>
2025-02-16 10:28:21 +0000

    Update MainContent.jsx
    function MainContent() {
      return (
        <main>
          <p>I love to visit New York, Paris, and Tokyo.</p>
        </main>
      );
    }
    
    export default MainContent;

--- alx-react-app/src/MainContent.jsx
+++ alx-react-app/src/MainContent.jsx
@@ -1,9 +1,9 @@
-function Header() {
-    return (
-      <header>
-        <h1>My Favorite Cities</h1>
-      </header>
-    );
-  }
-  
-  export default Header;
+function MainContent() {
+  return (
+    <main>
+      <p>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+}
+
+export default MainContent;
