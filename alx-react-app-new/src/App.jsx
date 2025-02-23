8bf9fc9426c447df9f49347b684db47bb0225999
thelma-github <thelmabuabeng4@gmail.com>
2025-02-23 23:59:20 +0000

    Update App.jsx
    function App() {
      return (
        <div>
          <h1 style={{ textAlign: "center" }}>React Counter Application</h1>
          <Counter />
        </div>
      );
    }
    
    export default App;

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,23 +1,10 @@
-// Import existing component
-import WelcomeMessage from "./components/WelcomeMessage";
-
-// Import new components
-import Header from "./components/Header";
-import MainContent from "./components/MainContent";
-import Footer from "./components/Footer";
-import UserProfile from "./components/UserProfile"; // Import UserProfile
-
-const App = () => {
-    return (
-        <div>
-            <WelcomeMessage />
-            <Header />
-            <MainContent />
-            {/* Adding UserProfile Component */}
-            <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
-            <Footer />
-        </div>
-    );
+function App() {
+  return (
+    <div>
+      <h1 style={{ textAlign: "center" }}>React Counter Application</h1>
+      <Counter />
+    </div>
+  );
 }
 
 export default App;
