25454af5dbcc1f8dd524a10c476c697fdb9f2cc1
thelma-github <thelmabuabeng4@gmail.com>
2025-02-24 00:07:15 +0000

    Update Navbar.jsx
    import { Link } from "react-router-dom";
    
    function Navbar() {
      return (
        <nav style={{
          backgroundColor: "navy",
          padding: "10px",
          textAlign: "center",
          display: "flex",
          justifyContent: "space-around",
          alignItems: "center"
        }}>
          <Link to="/" style={{ color: "white", textDecoration: "none" }}>Home</Link>
          <Link to="/about" style={{ color: "white", textDecoration: "none" }}>About</Link>
          <Link to="/services" style={{ color: "white", textDecoration: "none" }}>Services</Link>
          <Link to="/contact" style={{ color: "white", textDecoration: "none" }}>Contact</Link>
        </nav>
      );
    }
    
    export default Navbar;

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,20 +2,20 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav style={{ backgroundColor: "navy", padding: "10px", textAlign: "center" }}>
-      <Link to="/" style={linkStyle}>Home</Link>
-      <Link to="/about" style={linkStyle}>About</Link>
-      <Link to="/services" style={linkStyle}>Services</Link>
-      <Link to="/contact" style={linkStyle}>Contact</Link>
+    <nav style={{
+      backgroundColor: "navy",
+      padding: "10px",
+      textAlign: "center",
+      display: "flex",
+      justifyContent: "space-around",
+      alignItems: "center"
+    }}>
+      <Link to="/" style={{ color: "white", textDecoration: "none" }}>Home</Link>
+      <Link to="/about" style={{ color: "white", textDecoration: "none" }}>About</Link>
+      <Link to="/services" style={{ color: "white", textDecoration: "none" }}>Services</Link>
+      <Link to="/contact" style={{ color: "white", textDecoration: "none" }}>Contact</Link>
     </nav>
   );
 }
-
-const linkStyle = {
-  color: "white",
-  margin: "0 15px",
-  textDecoration: "none",
-  fontSize: "18px"
-};
 
 export default Navbar;
