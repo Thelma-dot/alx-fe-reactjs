70fc4147bf09ff9b4709f294af80ef658706a032
thelma-github <thelmabuabeng4@gmail.com>
2025-03-02 01:15:17 +0000

    Update recipeStore.js
    import { create } from "zustand";
    
    const useRecipeStore = create((set) => ({
      recipes: [],
      setRecipes: (newRecipes) => set({ recipes: newRecipes }), // ✅ Added setRecipes
      addRecipe: (newRecipe) =>
        set((state) => ({ recipes: [...state.recipes, newRecipe] })),
    }));
    
    export default useRecipeStore;

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -2,7 +2,9 @@ import { create } from "zustand";
 
 const useRecipeStore = create((set) => ({
   recipes: [],
-  addRecipe: (newRecipe) => set((state) => ({ recipes: [...state.recipes, newRecipe] })),
+  setRecipes: (newRecipes) => set({ recipes: newRecipes }), // ✅ Added setRecipes
+  addRecipe: (newRecipe) =>
+    set((state) => ({ recipes: [...state.recipes, newRecipe] })),
 }));
 
 export default useRecipeStore;
