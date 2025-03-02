e61565664800ce79179f9dc3dd6491cecebdd95c
thelma-github <thelmabuabeng4@gmail.com>
2025-03-02 21:52:11 +0000

    Create useRecipeStore.js
    import create from 'zustand';
    
    const useRecipeStore = create((set) => ({
      recipes: [],
      searchTerm: '',
      filteredRecipes: [],
      setSearchTerm: (term) =>
        set((state) => {
          const filtered = state.recipes.filter((recipe) =>
            recipe.title.toLowerCase().includes(term.toLowerCase())
          );
          return { searchTerm: term, filteredRecipes: filtered };
        }),
      setRecipes: (newRecipes) => set({ recipes: newRecipes, filteredRecipes: newRecipes }),
    }));
    
    export default useRecipeStore;

---
+++ recipe-sharing-app/src/components/useRecipeStore.js
@@ -0,0 +1,17 @@
+import create from 'zustand';
+
+const useRecipeStore = create((set) => ({
+  recipes: [],
+  searchTerm: '',
+  filteredRecipes: [],
+  setSearchTerm: (term) =>
+    set((state) => {
+      const filtered = state.recipes.filter((recipe) =>
+        recipe.title.toLowerCase().includes(term.toLowerCase())
+      );
+      return { searchTerm: term, filteredRecipes: filtered };
+    }),
+  setRecipes: (newRecipes) => set({ recipes: newRecipes, filteredRecipes: newRecipes }),
+}));
+
+export default useRecipeStore;
