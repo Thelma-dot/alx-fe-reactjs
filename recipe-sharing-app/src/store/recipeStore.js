483efdc558433d28f42a7c1f8c2fbe136216e3fe
thelma-github <thelmabuabeng4@gmail.com>
2025-03-02 02:58:58 +0000

    Update recipeStore.js
    import { create } from 'zustand';
    
    export const useRecipeStore = create((set) => ({
      recipes: [],
      addRecipe: (recipe) => set((state) => ({ recipes: [...state.recipes, recipe] })),
      updateRecipe: (updatedRecipe) =>
        set((state) => ({
          recipes: state.recipes.map((recipe) =>
            recipe.id === updatedRecipe.id ? updatedRecipe : recipe
          ),
        })),
      deleteRecipe: (recipeId) =>
        set((state) => ({ recipes: state.recipes.filter((recipe) => recipe.id !== recipeId) })),
    }));
    
    export default useRecipeStore;

--- recipe-sharing-app/src/store/recipeStore.js
+++ recipe-sharing-app/src/store/recipeStore.js
@@ -1,9 +1,16 @@
 import { create } from 'zustand';
 
-const useRecipeStore = create(set => ({
+export const useRecipeStore = create((set) => ({
   recipes: [],
-  addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
-  setRecipes: (recipes) => set({ recipes })
+  addRecipe: (recipe) => set((state) => ({ recipes: [...state.recipes, recipe] })),
+  updateRecipe: (updatedRecipe) =>
+    set((state) => ({
+      recipes: state.recipes.map((recipe) =>
+        recipe.id === updatedRecipe.id ? updatedRecipe : recipe
+      ),
+    })),
+  deleteRecipe: (recipeId) =>
+    set((state) => ({ recipes: state.recipes.filter((recipe) => recipe.id !== recipeId) })),
 }));
 
 export default useRecipeStore;
