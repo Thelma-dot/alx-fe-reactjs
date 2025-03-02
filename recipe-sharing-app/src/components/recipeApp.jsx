eed3c32b05aa23c02cdb7e8ca1d16d2128397d53
thelma-github <thelmabuabeng4@gmail.com>
2025-03-02 17:57:32 +0000

    Create recipeApp.jsx
    const RecipeApp = () => {
      return (
        <div>
          <h1>Recipe Sharing Application</h1>
          <SearchBar />
          <RecipeList />
        </div>
      );
    };
    
    export default RecipeApp;

---
+++ recipe-sharing-app/src/components/recipeApp.jsx
@@ -0,0 +1,11 @@
+const RecipeApp = () => {
+  return (
+    <div>
+      <h1>Recipe Sharing Application</h1>
+      <SearchBar />
+      <RecipeList />
+    </div>
+  );
+};
+
+export default RecipeApp;
