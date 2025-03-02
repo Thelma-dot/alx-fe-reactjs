8d1793b1e50e958ed27aa789d2eb64060c644cfd
thelma-github <thelmabuabeng4@gmail.com>
2025-03-02 02:04:11 +0000

    Update recipeStore.js
    import { create } from 'zustand';
    
    const useRecipeStore = create((set) => ({
      recipes: [],
    
      addRecipe: (newRecipe) => set((state) => ({
        recipes: [...state.recipes, newRecipe]
      })),
    
      updateRecipe: (id, updatedRecipe) => set((state) => ({
        recipes: state.recipes.map(recipe => 
          recipe.id === id ? { ...recipe, ...updatedRecipe } : recipe
        )
      })),
    
      deleteRecipe: (id) => set((state) => ({
        recipes: state.recipes.filter(recipe => recipe.id !== id)
      })),
    
      setRecipes: (newRecipes) => set({ recipes: newRecipes }) // ✅ Added setRecipes
    }));
    
    export default useRecipeStore;

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,22 +1,23 @@
-import { create } from "zustand";
+import { create } from 'zustand';
 
 const useRecipeStore = create((set) => ({
   recipes: [],
 
-  addRecipe: (newRecipe) =>
-    set((state) => ({ recipes: [...state.recipes, newRecipe] })),
+  addRecipe: (newRecipe) => set((state) => ({
+    recipes: [...state.recipes, newRecipe]
+  })),
 
-  updateRecipe: (id, updatedRecipe) =>
-    set((state) => ({
-      recipes: state.recipes.map((recipe) =>
-        recipe.id === id ? { ...recipe, ...updatedRecipe } : recipe
-      ),
-    })),
+  updateRecipe: (id, updatedRecipe) => set((state) => ({
+    recipes: state.recipes.map(recipe => 
+      recipe.id === id ? { ...recipe, ...updatedRecipe } : recipe
+    )
+  })),
 
-  deleteRecipe: (id) =>
-    set((state) => ({
-      recipes: state.recipes.filter((recipe) => recipe.id !== id),
-    })),
+  deleteRecipe: (id) => set((state) => ({
+    recipes: state.recipes.filter(recipe => recipe.id !== id)
+  })),
+
+  setRecipes: (newRecipes) => set({ recipes: newRecipes }) // ✅ Added setRecipes
 }));
 
 export default useRecipeStore;
