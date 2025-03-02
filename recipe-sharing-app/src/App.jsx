ea52fce762f6941729a05c5d4f2d4eaffb6d8a30
thelma-github <thelmabuabeng4@gmail.com>
2025-03-02 18:07:12 +0000

    Update App.jsx
    import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
    import RecipeList from './components/RecipeList';
    import AddRecipeForm from './components/AddRecipeForm';
    import RecipeDetails from './components/RecipeDetails';
    import SearchBar from './components/SearchBar';
    
    const App = () => {
      return (
        <Router>
          <div>
            <h1>Recipe Sharing App</h1>
            <SearchBar />
            <AddRecipeForm />
            <Routes>
              <Route path="/" element={<RecipeList />} />
              <Route path="/recipe/:recipeId" element={<RecipeDetails />} />
            </Routes>
          </div>
        </Router>
      );
    };
    
    export default App;

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,65 +1,23 @@
-import { useState } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import RecipeList from "./components/RecipeList";
-import AddRecipeForm from "./components/AddRecipeForm"; 
-import RecipeList from "./components/RecipeList"; 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import RecipeList from './components/RecipeList';
+import AddRecipeForm from './components/AddRecipeForm';
+import RecipeDetails from './components/RecipeDetails';
 import SearchBar from './components/SearchBar';
-import Home from "./pages/Home";
-import RecipeDetails from "./pages/RecipeDetails";
-
-const App = () => {
-  const [recipes, setRecipes] = useState([]);
-
-  const addRecipe = (newRecipe) => {
-    setRecipes([...recipes, newRecipe]);
-  };
-
-  return (
-    <div className="app">
-      <h1>Recipe Sharing App</h1>
-      <AddRecipeForm addRecipe={addRecipe} />
-      <RecipeList recipes={recipes} />
-    </div>
-  );
-};
-
-function App() {
-  return (
-    <div>
-      <SearchBar />
-      <RecipeList />
-    </div>
-  );
-}
 
 const App = () => {
   return (
     <Router>
-       <div>
+      <div>
         <h1>Recipe Sharing App</h1>
         <SearchBar />
-      <Routes>
-        <Route path="/" element={<RecipeList />} />
-        <Route path="/recipe/:id" element={<RecipeDetails />} />
-        <Route path="/add" element={<AddRecipeForm />} />
-      </Routes>
-         </div>
-    </Router>
-  );
-};
-
-function App() {
-  return (
-    <Router>
-      <div className="App">
+        <AddRecipeForm />
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/recipe/:id" element={<RecipeDetails />} />
+          <Route path="/" element={<RecipeList />} />
+          <Route path="/recipe/:recipeId" element={<RecipeDetails />} />
         </Routes>
       </div>
     </Router>
   );
-}
-
+};
 
 export default App;
