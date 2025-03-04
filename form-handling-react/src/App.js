1e1c6e19f7ae352035d409117d264ef65189fa15
thelma-github <thelmabuabeng4@gmail.com>
2025-03-04 22:22:02 +0000

    Create App.js
    import React from "react";
    import RegistrationForm from "./components/RegistrationForm";
    import FormikForm from "./components/FormikForm";
    
    const App = () => {
      return (
        <div>
          <h2>Controlled Component Form</h2>
          <RegistrationForm />
    
          <h2>Formik Form</h2>
          <FormikForm />
        </div>
      );
    };
    
    export default App;

---
+++ form-handling-react/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import RegistrationForm from "./components/RegistrationForm";
+import FormikForm from "./components/FormikForm";
+
+const App = () => {
+  return (
+    <div>
+      <h2>Controlled Component Form</h2>
+      <RegistrationForm />
+
+      <h2>Formik Form</h2>
+      <FormikForm />
+    </div>
+  );
+};
+
+export default App;
