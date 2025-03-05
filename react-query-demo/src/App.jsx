ad89d44a8a856747a1188dea471f2771391db17f
thelma-github <thelmabuabeng4@gmail.com>
2025-03-05 01:57:05 +0000

    Update App.jsx
    import React from "react";
    import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
    import PostsComponent from "./PostsComponent";
    
    const queryClient = new QueryClient();
    
    const App = () => {
      return (
        <QueryClientProvider client={queryClient}>
          <div>
            <h1>React Query Demo</h1>
            <PostsComponent />
          </div>
        </QueryClientProvider>
      );
    };
    
    export default App;

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -1,13 +1,17 @@
-// src/App.jsx
 import React from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import PostsComponent from "./PostsComponent";
+
+const queryClient = new QueryClient();
 
 const App = () => {
   return (
-    <div>
-      <h1>React Query Demo</h1>
-      <PostsComponent />
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div>
+        <h1>React Query Demo</h1>
+        <PostsComponent />
+      </div>
+    </QueryClientProvider>
   );
 };
 
