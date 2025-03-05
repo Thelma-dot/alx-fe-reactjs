a9339072b47bdf57de4bf31c5169ed5fdadfdfe6
thelma-github <thelmabuabeng4@gmail.com>
2025-03-05 02:42:52 +0000

    Update App.jsx
    import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
    import Layout from './components/Layout';
    import Home from './pages/Home';
    import Profile from './components/Profile';
    import ProfileDetails from './pages/ProfileDetails';
    import ProfileSettings from './pages/ProfileSettings';
    import BlogPost from './pages/BlogPost';
    import ProtectedRoute from './components/ProtectedRoute';
    
    export default function App() {
      return (
        <Router>
          <Routes>
            <Route path="/" element={<Layout />}>
              <Route index element={<Home />} />
              <Route path="profile" element={<ProtectedRoute><Profile /></ProtectedRoute>}>
                <Route path="details" element={<ProfileDetails />} />
                <Route path="settings" element={<ProfileSettings />} />
              </Route>
              <Route path="blog/:id" element={<BlogPost />} />
            </Route>
          </Routes>
        </Router>
      );
    }

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -17,7 +17,7 @@ export default function App() {
             <Route path="details" element={<ProfileDetails />} />
             <Route path="settings" element={<ProfileSettings />} />
           </Route>
-          <Route path="blog/:postId" element={<BlogPost />} />
+          <Route path="blog/:id" element={<BlogPost />} />
         </Route>
       </Routes>
     </Router>
