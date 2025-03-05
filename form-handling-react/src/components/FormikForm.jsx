2068e552821d2b1479a337ba441d4eec6456b79e
thelma-github <thelmabuabeng4@gmail.com>
2025-03-05 01:12:53 +0000

    Update and rename FormikForm.js to FormikForm.jsx
    import React from "react";
    import { Formik, Field, Form, ErrorMessage } from "formik";
    import * as Yup from "yup";
    
    const validationSchema = Yup.object({
      username: Yup.string().required("Username is required"),
      email: Yup.string().email("Invalid email").required("Email is required"),
      password: Yup.string().min(6, "Password must be at least 6 characters").required("Password is required"),
    });
    
    function FormikForm() {
      return (
        <Formik
          initialValues={{ username: "", email: "", password: "" }}
          validationSchema={validationSchema}
          onSubmit={(values) => {
            console.log("Form data", values);
          }}
        >
          {({ handleSubmit }) => (
            <Form onSubmit={handleSubmit}>
              <div>
                <label>Username:</label>
                <Field type="text" name="username" />
                <ErrorMessage name="username" component="p" className="error-message" />
              </div>
    
              <div>
                <label>Email:</label>
                <Field type="email" name="email" />
                <ErrorMessage name="email" component="p" className="error-message" />
              </div>
    
              <div>
                <label>Password:</label>
                <Field type="password" name="password" />
                <ErrorMessage name="password" component="p" className="error-message" />
              </div>
    
              <button type="submit">Submit</button>
            </Form>
          )}
        </Formik>
      );
    }
    
    export default FormikForm;

--- form-handling-react/src/components/FormikForm.jsx
+++ form-handling-react/src/components/FormikForm.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Formik, Form, Field, ErrorMessage } from "formik";
+import { Formik, Field, Form, ErrorMessage } from "formik";
 import * as Yup from "yup";
 
 const validationSchema = Yup.object({
@@ -8,40 +8,40 @@ const validationSchema = Yup.object({
   password: Yup.string().min(6, "Password must be at least 6 characters").required("Password is required"),
 });
 
-const FormikForm = () => {
+function FormikForm() {
   return (
     <Formik
       initialValues={{ username: "", email: "", password: "" }}
       validationSchema={validationSchema}
-      onSubmit={(values) => console.log("Form submitted:", values)}
+      onSubmit={(values) => {
+        console.log("Form data", values);
+      }}
     >
-      {({ isSubmitting }) => (
-        <Form>
+      {({ handleSubmit }) => (
+        <Form onSubmit={handleSubmit}>
           <div>
             <label>Username:</label>
             <Field type="text" name="username" />
-            <ErrorMessage name="username" component="p" style={{ color: "red" }} />
+            <ErrorMessage name="username" component="p" className="error-message" />
           </div>
 
           <div>
             <label>Email:</label>
             <Field type="email" name="email" />
-            <ErrorMessage name="email" component="p" style={{ color: "red" }} />
+            <ErrorMessage name="email" component="p" className="error-message" />
           </div>
 
           <div>
             <label>Password:</label>
             <Field type="password" name="password" />
-            <ErrorMessage name="password" component="p" style={{ color: "red" }} />
+            <ErrorMessage name="password" component="p" className="error-message" />
           </div>
 
-          <button type="submit" disabled={isSubmitting}>
-            Register
-          </button>
+          <button type="submit">Submit</button>
         </Form>
       )}
     </Formik>
   );
-};
+}
 
 export default FormikForm;
