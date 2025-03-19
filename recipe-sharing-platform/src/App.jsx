a8ebd4eca34605996f8da8be3acfafce2ef126ed
thelma-github <thelmabuabeng4@gmail.com>
2025-03-19 13:39:38 +0000

    Update App.jsx
    import Navbar from './components/Navbar';
    import { Routes, Route } from 'react-router-dom';
    import RecipeDetails from './pages/RecipeDetails';
    import AddRecipe from './pages/AddRecipe';
    import React from 'react';
    import HomePage from './components/HomePage';
    
    function App() {
      return (
        <div className="min-h-screen bg-gray-50">
          <Navbar />
          <Routes>
            <Route path="/" element={<HomePage />} />
            <Route path="/recipe/:id" element={<RecipeDetails />} />
            <Route path="/add" element={<AddRecipe />} />
          </Routes>
        </div>
      );
    }
    
    export default App;

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,15 +1,16 @@
 import Navbar from './components/Navbar';
 import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
 import RecipeDetails from './pages/RecipeDetails';
 import AddRecipe from './pages/AddRecipe';
+import React from 'react';
+import HomePage from './components/HomePage';
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-50">
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/recipe/:id" element={<RecipeDetails />} />
         <Route path="/add" element={<AddRecipe />} />
       </Routes>
