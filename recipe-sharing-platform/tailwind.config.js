77dd9dd1bdfbf43848484e0c0f6b8a99ba52a888
thelma-github <thelmabuabeng4@gmail.com>
2025-03-19 13:31:14 +0000

    Update tailwind.config.js
    /** @type {import('tailwindcss').Config} */
    export default {
      content: ["./public/index.html", "./src/**/*.{js,jsx,ts,tsx}"],
      theme: {
        extend: {},
      },
      plugins: [],
    };

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./public.index.html/src/**/*.{js,jsx,ts,tsx}'],
+  content: ["./public/index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
 };
+
