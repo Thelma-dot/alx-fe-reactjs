1b912b7075c9a328c209f603d2b27c1055d83bd7
thelma-github <thelmabuabeng4@gmail.com>
2025-03-30 10:38:47 +0000

    Update githubService.js
    import axios from "axios";
    
    export const fetchUserData = async (username) => {
      try {
        const response = await axios.get(`https://api.github.com/users/${username}`);
        return response.data;
      } catch (error) {
        throw new Error("User not found");
      }
    };

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,14 +1,9 @@
 import axios from "axios";
 
-export const fetchUsers = async (username, location, minRepos) => {
+export const fetchUserData = async (username) => {
   try {
-    let query = `q=${username}`;
-
-    if (location) query += `+location:${location}`;
-    if (minRepos) query += `+repos:>${minRepos}`;
-
-    const response = await axios.get(`https://api.github.com/search/users?${query}`);
-    return response.data.items;
+    const response = await axios.get(`https://api.github.com/users/${username}`);
+    return response.data;
   } catch (error) {
     throw new Error("User not found");
   }
